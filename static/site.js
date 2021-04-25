85b76b88d24b5560584861a46bb55a2431e9c976
Daniel M. Capella <polyzen@archlinux.org>
2021-04-25 07:21:33 -0400

    Bump Bootstrap to v5.0.0 (WIP)
    Glyphicons are no longer bundled in Bootstrap 4+. Rather than adding
    another dep, use system-provided emoji, which provide a native feel and
    are available practically everywhere: https://caniemoji.com/

--- static/site.js
+++ static/site.js
@@ -5,18 +5,18 @@ function hashSelect(first) {
     all_ids.forEach(idx => {
         var curr = document.querySelector('#' + idx);
         if (location.href.endsWith(SEP + idx)) {
-            curr.classList.remove('hidden');
+            curr.classList.remove('d-none');
             found = true;
         }
         else {
-            var isHidden = curr.classList.contains('hidden');
+            var isHidden = curr.classList.contains('d-none');
             if (! isHidden) {
-                curr.classList.add('hidden');
+                curr.classList.add('d-none');
             }
         }
     });
     if (! found) {
-        document.querySelector("#" + first).classList.remove('hidden');
+        document.querySelector("#" + first).classList.remove('d-none');
         history.pushState({}, '', location.href + SEP + first);
     }
 }
@@ -33,9 +33,9 @@ ready(() => {
     // Wire up the "yes" links
     document.querySelectorAll("a.yes").forEach((yes) => {
         yes.addEventListener("click", (e) => {
-            e.currentTarget.parentElement.parentElement.classList.add('hidden');
+            e.currentTarget.parentElement.parentElement.classList.add('d-none');
             var next = e.currentTarget.getAttribute('data-next');
-            document.querySelector('#' + next).classList.remove('hidden');
+            document.querySelector('#' + next).classList.remove('d-none');
             var original = location.href.replace(/\/$/, "");
             history.pushState({}, '', original + SEP + next);
         });
@@ -44,9 +44,9 @@ ready(() => {
     // Wire up the "nope" links
     document.querySelectorAll("a.nope").forEach((nope) => {
         nope.addEventListener("click", (e) => {
-            e.currentTarget.parentElement.parentElement.classList.add('hidden');
+            e.currentTarget.parentElement.parentElement.classList.add('d-none');
             var next = e.currentTarget.getAttribute('data-next');
-            document.querySelector('#' + next).classList.remove('hidden');
+            document.querySelector('#' + next).classList.remove('d-none');
             var tokens = location.href.replace(/\/$/, "").split(SEP).slice(0, -1);
             tokens.push(next);
             history.replaceState({}, '', tokens.join(SEP));
@@ -56,11 +56,11 @@ ready(() => {
     // Wire up the "back" links
     document.querySelectorAll("a.back").forEach((back) => {
         back.addEventListener("click", (e) => {
-            e.currentTarget.parentElement.parentElement.classList.add('hidden');
+            e.currentTarget.parentElement.parentElement.classList.add('d-none');
             var tokens = location.href.replace(/\/$/, "").split(SEP).slice(0, -1);
             var next = tokens.slice(-1).pop();
             history.go(-1);
-            document.querySelector('#' + next).classList.remove('hidden');
+            document.querySelector('#' + next).classList.remove('d-none');
         });
     });
     window.addEventListener('hashchange', function() {
