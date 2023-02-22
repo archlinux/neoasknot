03f2f41caa25dead5b5492dddc490db550b69254
Daniel M. Capella <polyzen@archlinux.org>
2023-02-22 00:57:43 -0500

    Make ChildGrid component (cont.)
    Fix title casing

--- src/lib/ChildGrid.svelte
+++ src/lib/ChildGrid.svelte
@@ -7,21 +7,21 @@
 </script>
 
 <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-  {#each children as { title, subtitle, link }}
-    {@const href = link || `${routePath}/${title}`}
+  {#each children as { title, subtitle, link, node }}
+    {@const href = link || `${routePath}/${node}`}
     {@const icon = link ? '🔗' : '📁'}
     <a {href} class="card card-hover flex flex-col">
       {#if subtitle}
         <article class="space-y-4 p-4">
           <div class="flex place-content-between">
-            <h2 class="inline capitalize">{title}</h2>
+            <h2 class="inline">{title}</h2>
             <span class="mt-1 md:mt-1.5">{icon}</span>
           </div>
           <p>{subtitle}</p>
         </article>
       {:else}
         <div class="flex place-content-between p-4">
-          <h2 class="inline capitalize">{title}</h2>
+          <h2 class="inline">{title}</h2>
           <span class="mt-1 md:mt-1.5">{icon}</span>
         </div>
       {/if}
