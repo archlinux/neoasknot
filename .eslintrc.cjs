daa3d8a9cb2b88f2edd6ef9fe77ff86dfcb056b5
Daniel M. Capella <polyzen@archlinux.org>
2023-03-14 22:57:31 -0400

    Switch to eslint-plugin-yml from yamllint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:yml/standard',
     'prettier',
   ],
+  rules: {
+    'yml/quotes': ['error', { prefer: 'single' }],
+  },
   plugins: ['svelte3', '@typescript-eslint'],
   ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  overrides: [
+    { files: ['*.svelte'], processor: 'svelte3/svelte3' },
+    { files: ['*.yaml', '*.yml'], parser: 'yaml-eslint-parser' },
+  ],
   settings: {
     'svelte3/typescript': () => require('typescript'),
   },
