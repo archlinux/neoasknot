73da04ee91d434705de23a25d514f52a5076d230
Daniel M. Capella <polyzen@archlinux.org>
2025-08-11 14:40:55 -0400

    Use Vite's defineConfig helper

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
 
-export default {
+export default defineConfig({
   plugins: [dynamicImport(), sveltekit(), tailwindcss(), ViteYaml()],
-} satisfies UserConfig;
+});
