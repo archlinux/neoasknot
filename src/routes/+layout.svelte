8809edc51e25a01b43af2934b918c7ec715de158
Daniel M. Capella <polyzen@archlinux.org>
2025-08-16 16:32:21 -0400

    Merge $content/{lib,static}
    The `static` dir was no longer acting as a Vite public directory (as of
    8944eca2), and its contents are being imported now anyways.
    
    https://vite.dev/guide/assets.html#the-public-directory

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import '../app.css';
+  import favicon from '$content/lib/favicon.png';
+  import logo from '$content/lib/logo.svg';
   import '$content/lib/org.css';
-  import favicon from '$content/static/favicon.png';
-  import logo from '$content/static/logo.svg';
   import { page } from '$app/state';
   import { PUBLIC_ORG } from '$env/static/public';
 
